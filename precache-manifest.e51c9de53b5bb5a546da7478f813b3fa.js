0f937564d949563a3b5457149fe99731fad25dbf
h2ash <hun22ash@gmail.com>
2019-07-22 22:06:38 +0300

    Updates

--- precache-manifest.e51c9de53b5bb5a546da7478f813b3fa.js
+++ precache-manifest.e51c9de53b5bb5a546da7478f813b3fa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "2c94677ad00348390da71ddb237a2dbc",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "963b09522bf16b461d7c",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react_todo-app/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "963b09522bf16b461d7c",
+    "url": "/react_todo-app/static/js/main.8cc1caab.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
